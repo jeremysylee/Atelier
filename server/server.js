21324e3921363bb98733c58115455915bafac25f
Jeremy Lee <jeremysylee@gmail.com>
2021-07-01 15:30:59 -1000

    Updating files lost in the last rollback

--- server/server.js
+++ server/server.js
@@ -1,16 +1,6 @@
-const express = require('express');
-
-const cors = require('cors');
-const morgan = require('morgan');
-const router = require('./router');
-
+const app = require('./index');
 const port = 3001;
-const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(morgan('dev'));
-app.use('/api', router);
-
-module.exports = app;
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
